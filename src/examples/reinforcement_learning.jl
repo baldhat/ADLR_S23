42352303ce6bb41f40a30df692900540245b91db
baldhat <baldhat@web.de>
2023-04-29 17:47:45 +0200

    Fixed minor issues to allow a training to run

--- src/examples/reinforcement_learning.jl
+++ src/examples/reinforcement_learning.jl
@@ -18,7 +18,7 @@ using Statistics;
 using BSON: @save, @load # save mode
 
 
-create_visualization();
+Flyonic.Visualization.create_visualization();
 
 mutable struct VtolEnv{A,T,ACT,R<:AbstractRNG} <: AbstractEnv # Parametric Constructor for a subtype of AbstractEnv
     action_space::A
@@ -72,9 +72,9 @@ function VtolEnv(;
             ], 
     )
     
-    create_VTOL(name, actuators = true, color_vec=[1.0; 1.0; 0.6; 1.0]);
-    set_transform(name, [0.0; 0.0; 0.0] ,QuatRotation(UnitQuaternion(RotY(-pi/2.0)*RotX(pi))));
-    set_actuators(name, [0.0; 0.0; 0.0; 0.0])
+    Flyonic.Visualization.create_VTOL(name, actuators = true, color_vec=[1.0; 1.0; 0.6; 1.0]);
+    Flyonic.Visualization.set_transform(name, [0.0; 0.0; 0.0] ,QuatRotation(UnitQuaternion(RotY(-pi/2.0)*RotX(pi))));
+    Flyonic.Visualization.set_actuators(name, [0.0; 0.0; 0.0; 0.0])
 
 
     environment = VtolEnv(
@@ -88,12 +88,11 @@ function VtolEnv(;
         name,
         zeros(T, 3), # x_W
         zeros(T, 3), # v_B
-        Matrix(UnitQuaternion(RotY(-pi/2.0)*RotX(pi))), # Float64... so T needs to be Float64
+        Matrix(Rotations.UnitQuaternion(RotY(-pi/2.0)*RotX(pi))), # Float64... so T needs to be Float64
         zeros(T, 3), # ω_B
         zeros(T, 3), # wind_W
         T(0.025), # Δt  
     )
-    
     
     reset!(environment)
     
@@ -126,8 +125,8 @@ RLBase.reward(env::VtolEnv{A,T}) where {A,T} = computeReward(env)
 function RLBase.reset!(env::VtolEnv{A,T}) where {A,T}
     
     # Visualize initial state
-    set_transform(env.name, env.x_W,QuatRotation(env.R_W));
-    set_actuators(env.name, [0.0; 0.0; 0.0; 0.0])
+    Flyonic.Visualization.set_transform(env.name, env.x_W, QuatRotation(env.R_W));
+    Flyonic.Visualization.set_actuators(env.name, [0.0; 0.0; 0.0; 0.0])
     
     env.x_W = [0.0; 0.0; 0.0];
     env.v_B = [0.0; 0.0; 0.0];
@@ -159,17 +158,17 @@ end
 function _step!(env::VtolEnv, next_action)
         
     # caluclate wind impact
-    v_in_wind_B = vtol_add_wind(env.v_B, env.R_W, env.wind_W)
+    v_in_wind_B = Flyonic.RigidBodies.vtol_add_wind(env.v_B, env.R_W, env.wind_W)
     # caluclate aerodynamic forces
-    torque_B, force_B = vtol_model(v_in_wind_B, next_action, eth_vtol_param);
+    torque_B, force_B = Flyonic.VtolModel.vtol_model(v_in_wind_B, next_action, Flyonic.eth_vtol_param);
     # integrate rigid body dynamics for Δt
-    env.x_W, env.v_B, env.R_W, env.ω_B, time = rigid_body_simple(torque_B, force_B, env.x_W, env.v_B, env.R_W, env.ω_B, env.t, env.Δt, eth_vtol_param)
-
+    env.x_W, env.v_B, _, env.R_W, env.ω_B, _, env.t = Flyonic.RigidBodies.rigid_body_simple(torque_B, force_B, env.x_W, env.v_B, env.R_W, env.ω_B, env.t, env.Δt, Flyonic.eth_vtol_param)
+    
 
     # Visualize the new state 
     # TODO: Can be removed for real trainings
-    set_transform(env.name, env.x_W, QuatRotation(env.R_W));
-    set_actuators(env.name, next_action)
+    Flyonic.Visualization.set_transform(env.name, env.x_W, QuatRotation(env.R_W));
+    Flyonic.Visualization.set_actuators(env.name, next_action)
  
     env.t += env.Δt
     
@@ -195,15 +194,15 @@ end;
 
 seed = 123    
 rng = StableRNG(seed)
-    N_ENV = 8
-    UPDATE_FREQ = 1024
+N_ENV = 8
+UPDATE_FREQ = 1024
     
     
-    # define multiple environments for parallel training
-    env = MultiThreadEnv([
-        # use different names for the visualization
-        VtolEnv(; rng = StableRNG(hash(seed+i)), name = "vtol$i") for i in 1:N_ENV
-    ])
+# define multiple environments for parallel training
+env = MultiThreadEnv([
+    # use different names for the visualization
+    VtolEnv(; rng = StableRNG(hash(seed+i)), name = "vtol$i") for i in 1:N_ENV
+])
 
 
 
@@ -211,67 +210,67 @@ rng = StableRNG(seed)
 # Define the function approximator
 ns, na = length(state(env[1])), length(action_space(env[1]))
 approximator = ActorCritic(
-            actor = GaussianNetwork(
-                pre = Chain(
-                Dense(ns, 16, relu; initW = glorot_uniform(rng)),#
-                Dense(16, 16, relu; initW = glorot_uniform(rng)),
-                ),
-                μ = Chain(Dense(16, na; initW = glorot_uniform(rng))),
-                logσ = Chain(Dense(16, na; initW = glorot_uniform(rng))),
-            ),
-            critic = Chain(
-                Dense(ns, 16, relu; initW = glorot_uniform(rng)),
-                Dense(16, 16, relu; initW = glorot_uniform(rng)),
-                Dense(16, 1; initW = glorot_uniform(rng)),
-            ),
-            optimizer = ADAM(1e-3),
-        );
-
-
-
-        agent = Agent( # A wrapper of an AbstractPolicy
-        # AbstractPolicy: the policy to use
-        policy = PPOPolicy(;
-                    approximator = approximator |> gpu,
-                    update_freq=UPDATE_FREQ,
-                    dist = Normal,
-                    # For parameters visit the docu: https://juliareinforcementlearning.org/docs/rlzoo/#ReinforcementLearningZoo.PPOPolicy
-                    ),
-        
-        # AbstractTrajectory: used to store transitions between an agent and an environment source
-        trajectory = PPOTrajectory(;
-            capacity = UPDATE_FREQ,
-            state = Matrix{Float64} => (ns, N_ENV),
-            action = Matrix{Float64} => (na, N_ENV),
-            action_log_prob = Vector{Float64} => (N_ENV,),
-            reward = Vector{Float64} => (N_ENV,),
-            terminal = Vector{Bool} => (N_ENV,),
+    actor = GaussianNetwork(
+        pre = Chain(
+        Dense(ns, 16, relu; initW = glorot_uniform(rng)),#
+        Dense(16, 16, relu; initW = glorot_uniform(rng)),
         ),
-    );
-
-
-    function saveModel(t, agent, env)
-        model = cpu(agent.policy.approximator)   
-        f = joinpath("./RL_models/", "vtol_ppo_2_$t.bson")
-        @save f model
-        println("parameters at step $t saved to $f")
-    end
-
-
-    function loadModel()
-        f = joinpath("./src/examples/RL_models/", "vtol_ppo_2_9320000.bson")
-        @load f model
-        return model
-    end
+        μ = Chain(Dense(16, na; initW = glorot_uniform(rng))),
+        logσ = Chain(Dense(16, na; initW = glorot_uniform(rng))),
+    ),
+    critic = Chain(
+        Dense(ns, 16, relu; initW = glorot_uniform(rng)),
+        Dense(16, 16, relu; initW = glorot_uniform(rng)),
+        Dense(16, 1; initW = glorot_uniform(rng)),
+    ),
+    optimizer = ADAM(1e-3),
+);
 
 
 
-    agent.policy.approximator = loadModel();
+agent = Agent( # A wrapper of an AbstractPolicy
+    # AbstractPolicy: the policy to use
+    policy = PPOPolicy(;
+                approximator = approximator |> gpu,
+                update_freq=UPDATE_FREQ,
+                dist = Normal,
+                # For parameters visit the docu: https://juliareinforcementlearning.org/docs/rlzoo/#ReinforcementLearningZoo.PPOPolicy
+                ),
+
+    # AbstractTrajectory: used to store transitions between an agent and an environment source
+    trajectory = PPOTrajectory(;
+        capacity = UPDATE_FREQ,
+        state = Matrix{Float64} => (ns, N_ENV),
+        action = Matrix{Float64} => (na, N_ENV),
+        action_log_prob = Vector{Float64} => (N_ENV,),
+        reward = Vector{Float64} => (N_ENV,),
+        terminal = Vector{Bool} => (N_ENV,),
+    ),
+);
 
 
-    run(
-           agent,
-           env,
-           StopAfterStep(100_000),
-           DoEveryNStep(saveModel, n=40_000)
-       )
+function saveModel(t, agent, env)
+    model = cpu(agent.policy.approximator)   
+    f = joinpath("./RL_models/", "vtol_ppo_2_$t.bson")
+    @save f model
+    println("parameters at step $t saved to $f")
+end
+
+
+function loadModel()
+    f = joinpath("./src/examples/RL_models/", "vtol_ppo_2_9320000.bson")
+    @load f model
+    return model
+end
+
+
+
+#agent.policy.approximator = loadModel();
+
+
+run(
+        agent,
+        env,
+        StopAfterStep(100_000),
+        DoEveryNStep(saveModel, n=40_000)
+    )
